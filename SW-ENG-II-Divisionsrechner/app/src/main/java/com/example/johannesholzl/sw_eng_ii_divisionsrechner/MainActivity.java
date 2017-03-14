17aadea20ceaeeed7eaa67796e48fcda6a18ecbe
Johannes Hölzl <johannes.hoelzl@gmail.com>
2017-03-14 10:54:23 +0100

    Initialer Commit
    Es wurde ein AndroidStudio-Projekt erstellt und das GUI gestaltet. Dabei ist noch kein Anzeichen von Funktionalität erkennbar. 
    
    Tastatur limitiert Eingabe auf Gleitkommazahlen, wobei diese auch negativ sein können.

---
+++ SW-ENG-II-Divisionsrechner/app/src/main/java/com/example/johannesholzl/sw_eng_ii_divisionsrechner/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.johannesholzl.sw_eng_ii_divisionsrechner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
