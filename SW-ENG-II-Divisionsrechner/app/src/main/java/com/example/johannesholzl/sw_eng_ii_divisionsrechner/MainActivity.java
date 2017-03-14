8657686ef6b508f780e02b6438297e817bdca470
Johannes Hölzl <johannes.hoelzl@gmail.com>
2017-03-14 11:01:09 +0100

    Funktionalität hinzugefügt
    Der Rechner kann nun divideren. Bei ungültiger (leerer) Eingabe stürzt das Programm ab. Auch wird die Division durch 0 nicht explizit behandelt.

--- SW-ENG-II-Divisionsrechner/app/src/main/java/com/example/johannesholzl/sw_eng_ii_divisionsrechner/MainActivity.java
+++ SW-ENG-II-Divisionsrechner/app/src/main/java/com/example/johannesholzl/sw_eng_ii_divisionsrechner/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.johannesholzl.sw_eng_ii_divisionsrechner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText dividend;
+    EditText divisor;
+    TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dividend = (EditText) findViewById(R.id.dividendTV);
+        divisor = (EditText) findViewById(R.id.divisorTV);
+        output = (TextView) findViewById(R.id.outputText);
+    }
+
+    public void onClickButton(View view){
+        double d0 = Double.parseDouble(dividend.getText().toString());
+        double d1 = Double.parseDouble(divisor.getText().toString());
+        output.setText(""+d0/d1);
     }
 }
